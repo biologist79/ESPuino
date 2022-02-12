2602f5fdf7e8481b3f9d097fc3cea79e84faf4b9
biologist79 <devel@espuino.de>
2022-02-13 00:40:39 +0100

    Adding new HAL for D32_SDMMC

--- src/settings-lolin_d32_sdmmc_pe.h
+++ src/settings-lolin_d32_sdmmc_pe.h
@@ -1,15 +1,15 @@
-#ifndef __ESPUINO_SETTINGS_LOLIN_D32_PRO_H__
-#define __ESPUINO_SETTINGS_LOLIN_D32_PRO_H__
+#ifndef __ESPUINO_SETTINGS_LOLIN_D32_H__
+#define __ESPUINO_SETTINGS_LOLIN_D32_H__
     #include "Arduino.h"
 
     //######################### INFOS ####################################
-    /* This is a PCB-specific config-file for *Wemos Lolin32 D32 pro with port-expander PCA9555PW and SD_MMC*.
+    /* This is a PCB-specific config-file for *Wemos Lolin32 D32 with port-expander PCA9555PW and SD_MMC*.
     PCB: t.b.a.
     Forum: https://forum.espuino.de/t/espuino-minid32-pro-lolin-d32-pro-mit-sd-mmc-und-port-expander-smd/866
-    Infos: https://www.wemos.cc/en/latest/d32/d32_pro.html
-    Schematics Lolin D32 pro: https://www.wemos.cc/en/latest/_static/files/sch_d32_pro_v2.0.0.pdf
+    Infos: https://www.wemos.cc/en/latest/d32/d32.html
+    Schematics Lolin D32: https://www.wemos.cc/en/latest/_static/files/sch_d32_v1.0.0.pdf
     Schematics PCB: t.b.a.
-    Caveats: GPIO35 (battery monitoring) + don't use internal SD-slot as it's to capable of SD_MMC because of its pin-layout!
+    Caveats: GPIO35 (battery monitoring). GPIOs 16+17 are not used by the PCB.
     Status:
         tested with PN5180 + SD_MMC (by biologist79)
     */
@@ -18,7 +18,6 @@
     // Please note: GPIOs 34, 35, 36, 39 are input-only and don't have internal pullup-resistors.
     // So if connecting a button to these, make sure to add a 10k-pullup-resistor for each button.
     // Further infos: https://randomnerdtutorials.com/esp32-pinout-reference-gpios/
-    // GPIOs 16+17 are not available for D32 pro as they're used to internal purposes (PSRAM).
     // All GPIOs >=100 and <= 115 are connected to a port-expander
     #ifdef SD_MMC_1BIT_MODE
         //  (MOSI)    15  CMD
@@ -88,9 +87,9 @@
     #define WAKEUP_BUTTON                   36          // Defines the button that is used to wake up ESPuino from deepsleep; set to 99 to disable
 
     // Power-control
-    #define POWER                           32          // GPIO used to drive transistor-circuit, that switches off peripheral devices while ESP32-deepsleep
+    #define POWER                           115         // GPIO used to drive transistor-circuit, that switches off peripheral devices while ESP32-deepsleep
     #ifdef POWER
-        //#define INVERT_POWER                          // If enabled, use inverted logic for POWER circuit, that means peripherals are turned off by writing HIGH
+        #define INVERT_POWER                            // If enabled, use inverted logic for POWER circuit, that means peripherals are turned off by writing HIGH
     #endif
 
     // (optional) Neopixel
