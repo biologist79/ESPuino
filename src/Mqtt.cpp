7936acb0214a49e7c06acdf5aa1f9fc2b69ff5e9
biologist79 <devel@espuino.de>
2022-10-14 23:28:18 +0200

    Fix for compile-error if no mqtt-support is included

--- src/Mqtt.cpp
+++ src/Mqtt.cpp
@@ -20,14 +20,13 @@
 #ifdef MQTT_ENABLE
     static WiFiClient Mqtt_WifiClient;
     static PubSubClient Mqtt_PubSubClient(Mqtt_WifiClient);
+    // Please note: all of them are defaults that can be changed later via GUI
+    String gMqttClientId = DEVICE_HOSTNAME; // ClientId for the MQTT-server, must be server wide unique (if not found in NVS this one will be taken)
+    String gMqttServer = "192.168.2.43";    // IP-address of MQTT-server (if not found in NVS this one will be taken)
+    String gMqttUser = "mqtt-user";         // MQTT-user
+    String gMqttPassword = "mqtt-password"; // MQTT-password
+    uint16_t gMqttPort = 1883;              // MQTT-Port
 #endif
-
-// Please note: all of them are defaults that can be changed later via GUI
-String gMqttClientId = DEVICE_HOSTNAME; // ClientId for the MQTT-server, must be server wide unique (if not found in NVS this one will be taken)
-String gMqttServer = "192.168.2.43";    // IP-address of MQTT-server (if not found in NVS this one will be taken)
-String gMqttUser = "mqtt-user";         // MQTT-user
-String gMqttPassword = "mqtt-password"; // MQTT-password
-uint16_t gMqttPort = 1883;              // MQTT-Port
 
 // MQTT
 static bool Mqtt_Enabled = true;
