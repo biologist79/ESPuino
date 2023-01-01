75d5e67c40115c28c6ba5e41543694ec3e424928
Frieder Schrempf <frieder@fris.de>
2023-01-01 23:34:16 +0100

    Kconfig.py: Generate PlatformIO configuration override

--- Kconfig.py
+++ Kconfig.py
@@ -15,7 +15,12 @@ def generate_configs(*args, **kwargs):
     kconf = kconfiglib.Kconfig("Kconfig")
     kconf.load_config()
     filename = "config.h"
+    pio_override_filename = "platformio-kconfig.ini"
     chunks = kconf._autoconf_contents(None).splitlines()
+    pio_chunks = [
+        '# DO NOT EDIT: This file is automatically created by Kconfig.py',
+        '[env:default]'
+    ]
 
     #
     # Kconfig can only deal with literals, not with identifiers. For some purposes
@@ -26,6 +31,9 @@ def generate_configs(*args, **kwargs):
     # To enable the automatic translation, make the config item depend on the
     # CONVERT_TO_IDENTIFIER item in the Kconfig file.
     #
+    # Similar to this, also collect symbols with a dependency to EXPORT_TO_PIO.
+    # They will be written to a config override file for PlatformIO.
+    #
     for symbol in kconf.unique_defined_syms:
         if not symbol._write_to_conf or not symbol.orig_type is kconfiglib.STRING:
             continue
@@ -35,6 +43,16 @@ def generate_configs(*args, **kwargs):
                     if chunks[i].startswith(f"#define {kconf.config_prefix}{symbol.name} "):
                         chunks[i] = chunks[i].replace('"', '')
                         break
+            if dep.name == "EXPORT_TO_PIO" and symbol.name and symbol.str_value:
+                pio_chunks.append(f"{symbol.name.lower().lstrip('pio_')} = {symbol.str_value}")
+
+    #
+    # Save the PlatformIO configuration override to a file.
+    #
+    if kconf._write_if_changed(pio_override_filename, "\n".join(pio_chunks)):
+        print("PlatformIO override saved to '{}'".format(pio_override_filename))
+    else:
+        print("No change to PlatformIO override in '{}'".format(pio_override_filename))
 
     #
     # Save the manipulated config header lines to config.h again replicating the
@@ -53,5 +71,6 @@ env.AddCustomTarget("defconfig", None, ['defconfig', generate_configs])
 env.AddCustomTarget("genconfig", None, [generate_configs])
 env.AddCustomTarget("guiconfig", None, ['guiconfig', generate_configs])
 env.AddCustomTarget("menuconfig", None, ['menuconfig', generate_configs])
-env.AddCustomTarget("mrproper", None, 'rm -f .config config.h')
+env.AddCustomTarget("mrproper", None, 'rm -f .config config.h platformio-kconfig.ini')
 env.AddCustomTarget("savedefconfig", None, ['savedefconfig'])
+
