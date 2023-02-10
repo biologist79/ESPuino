b0561f2e4232207d19cef742d46136f9d32da356
Laszlo Hegedüs <laszlo.hegedues@gmail.com>
2023-02-10 20:02:50 +0100

    Bugfix for automatic file generation
    Fixed some errors in the accesspoint html files, which created
    double text entries.
    
    Modifed processHtml.py to automatically generate  the needed files.
    
    Following functions are implemented:
     - listed HTML files will be minified but not compressed
     - Files ending in json will be minified & compressed
     - Every other file will be compressed without further post processing
    
    "Flask_Minify" is used as html  and "json" as json minifier.
    
    dist folder is removed since the source folder is used directly.

--- processHtml.py
+++ processHtml.py
@@ -7,23 +7,36 @@ Use this script for creating PROGMEM header files from html files.
 from pathlib import Path
 import os
 import mimetypes
+import gzip
+
+try:
+    from flask_minify.parsers import Parser
+except ImportError:
+  print("Trying to Install required module: flask_minify\nIf this failes, please execute \"pip install flask_minify\" manually.")
+  os.system('python -m pip install flask_minify')
+
+from flask_minify.parsers import Parser
+import json
 
 Import("env")  # pylint: disable=undefined-variable
 
 OUTPUT_DIR = (
     Path(env.subst("$BUILD_DIR")) / "generated"
 )  # pylint: disable=undefined-variable
-HTML_DIR = Path("dist").absolute()
+HTML_DIR = Path("html").absolute()
+# List of files, which will only be minifed but not compressed (f.e. html files with templates)
 WWW_FILES = [
     Path("management.html"),
     Path("accesspoint.html"),
 ]
+# list of all files, which shall be compressed before embedding
+# files with ".json" ending will be minifed before compression, ".js" will not be changed!
 BINARY_FILES =[
-    Path("js/i18next.min.js.gz"),
-    Path("js/i18nextHttpBackend.min.js.gz"),
-    Path("js/loc_i18next.min.js.gz"),
-    Path("locales/de.json.gz"),
-    Path("locales/en.json.gz")
+    Path("js/i18next.min.js"),
+    Path("js/i18nextHttpBackend.min.js"),
+    Path("js/loc_i18next.min.js"),
+    Path("locales/de.json"),
+    Path("locales/en.json")
 ]
 
 
@@ -33,7 +46,7 @@ class HtmlHeaderProcessor:
     @staticmethod
     def _escape_html(data):
         """Escape HTML characters for usage in C"""
-        data = data.replace("\n", "\\\n")
+        data = data.replace("\n", "\\n")
         data = data.replace('"', r"\"")
         data = data.replace(r"\d", r"\\d")
         data = data.replace(r"\.", r"\\.")
@@ -43,36 +56,50 @@ class HtmlHeaderProcessor:
 
     @classmethod
     def _process_header_file(cls, html_path, header_path):
+        parser = Parser({}, True)
+        parser.update_runtime_options(True, True, True)
         with html_path.open("r") as html_file:
-            content = cls._escape_html(html_file.read())
+            content = html_file.read()
+            content = parser.minify(content, "html")    # minify content as html
+            content = cls._escape_html(content)
 
         with header_path.open("w") as header_file:
             header_file.write(
                 f"static const char {html_path.name.split('.')[0]}_HTML[] PROGMEM = \""
             )
             header_file.write(content)
-            header_file.write('";')
+            header_file.write('";\n')
         header_file.close()
 
     @classmethod
     def _process_binary_file(cls, binary_path, header_path, info):
-        with binary_path.open("rb") as f:
-            content = f.read()
+        # minify json files explicitly
+        if binary_path.suffix == "json":
+            with binary_path.open("r") as f:
+                jsonObj = json.load(f)
+                content = json.dumps(jsonObj, separators=(',', ':'))
+        # use everything else as is
+        else:
+            with binary_path.open("r") as f:
+                content = f.read()
+
+        # compress content
+        data = gzip.compress(content.encode())
 
         with header_path.open("a") as header_file:
-            path = binary_path.relative_to(HTML_DIR).as_posix()
-            path = path[:-len(".gz")]
             varName = binary_path.name.split('.')[0]
             header_file.write(
-                f"static const uint8_t {varName}_BIN[] PROGMEM = {{"
+                f"static const uint8_t {varName}_BIN[] PROGMEM = {{\n    "
             )
             size = 0
-            for d in content:
+            for d in data:
+                # write out the compressed byte stream as a hex array and create a newline after every 20th entry
                 header_file.write("0x{:02X},".format(d))
+                size = size + 1
                 if not (size % 20):
                     header_file.write("\n    ")
-                size = size + 1
             header_file.write("\n};\n\n")
+            # populate dict with our information
             info["size"] = size
             info["variable"] = f"{varName}_BIN"
             return info
@@ -86,29 +113,28 @@ class HtmlHeaderProcessor:
             cls._process_header_file(HTML_DIR / html_file, OUTPUT_DIR / header_file)
         binary_header = OUTPUT_DIR / "HTMLbinary.h"
         if binary_header.exists():
-            os.remove(binary_header)
+            os.remove(binary_header)    # remove file if it exists, since we are appending to it
 
-        files = []
+        fileList = []   # dict holding the array of metadata for all processed binary files
         for binary_file in BINARY_FILES:
-            fp = HTML_DIR / binary_file
-            print(f"  {fp} -> {binary_header}")
-            # we always deal with compressed files here
-            uri = fp.relative_to(HTML_DIR).as_posix()
-            uri = uri[:-len(".gz")]
-            info = dict()
-            info["uri"] = "/" + uri
-            info["mimeType"] = mimetypes.types_map[Path(uri).suffix]
+            filePath = HTML_DIR / binary_file
+            print(f"  {filePath} -> {binary_header}")
+
+            info = dict()   # the dict entry for this file
+            info["uri"] = "/" + filePath.relative_to(HTML_DIR).as_posix()
+            info["mimeType"] = mimetypes.types_map[filePath.suffix]
             info = cls._process_binary_file(HTML_DIR / binary_file, binary_header, info)
-            files.append(info)
+            fileList.append(info)
 
         with binary_header.open("a") as f:
-            f.write("""typedef std::function<void(const String& uri, const String& contentType, const uint8_t * content, size_t len)> RouteRegistrationHandler;
+            f.write("""using RouteRegistrationHandler = std::function<void(const String& uri, const String& contentType, const uint8_t * content, size_t len)>;
 
 class WWWData {
     public:
         static void registerRoutes(RouteRegistrationHandler handler) {
 """)
-            for info in files:
+            for info in fileList:
+                # write the fileList entries to the binary file. These will be the paramenter with which the handler is called to register the endpoint on the webserver
                 f.write(f'            handler("{info["uri"]}", "{info["mimeType"]}", {info["variable"]}, {info["size"]});\n')
             f.write("        }\n};\n")
 
