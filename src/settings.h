b5671dfd8b539728d5b41d6fb6f32cdc34961704
Joe91 <1015.5@gmx.de>
2023-02-11 11:36:12 +0100

    Led improvements (#197)
    * initial commit
    
    * fix invalid line
    
    * complete rework
    
    * initial commit
    
    * fix dithering on dimmed LEDs
    
    * clean cast and rename of dim-function
    
    * rename some vars
    
    * remove special handling of volume
    
    * rework playlist-animation
    
    * some small cleanup
    
    * separate more animations. remove obsolte logic
    
    * cleanup
    
    * more cleanup and some bugfixes
    
    * fix bluetooth color for Sink-Mode
    
    * some more cleanup. Undo wrong "bluetooth-fix"
    
    * fix for filesize 0
    
    * start to add review of  laszloh
    
    * more review items
    
    * more review items done
    
    * always execute battery-measurement
    
    * more review from laszloh

--- src/settings.h
+++ src/settings.h
@@ -196,11 +196,14 @@
     //#################### Settings for optional Modules##############################
     // (optinal) Neopixel
     #ifdef NEOPIXEL_ENABLE
-        #define NUM_LEDS                    24          // number of LEDs
-        #define CHIPSET                     WS2812B     // type of Neopixel
-        #define COLOR_ORDER                 GRB
-        #define PROGRESS_HUE_START          85          // Start and end hue of mulitple-LED progress indicator. Hue ranges from basically 0 - 255, but you can also set numbers outside this range to get the desired effect (e.g. 85-215 will go from green to purple via blue, 341-215 start and end at exactly the same color but go from green to purple via yellow and red)
-        #define PROGRESS_HUE_END            -1
+		#define NUM_LEDS                    24          // number of LEDs
+		#define CHIPSET                     WS2812B     // type of Neopixel
+		#define COLOR_ORDER                 GRB
+		#define NUM_LEDS_IDLE_DOTS          5           // count of LEDs, which are shown when Idle
+		#define OFFSET_PAUSE_LEDS			false		// if true the pause-leds are centered in the mid of the LED-Strip
+		#define PROGRESS_HUE_START          85          // Start and end hue of mulitple-LED progress indicator. Hue ranges from basically 0 - 255, but you can also set numbers outside this range to get the desired effect (e.g. 85-215 will go from green to purple via blue, 341-215 start and end at exactly the same color but go from green to purple via yellow and red)
+		#define PROGRESS_HUE_END            -1
+		#define DIMMABLE_STATES				50			// Number of dimmed values between two full LEDs
         //#define LED_OFFSET                0           // shifts the starting LED in the original direction of the neopixel ring
     #endif
 
