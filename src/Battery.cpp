cbd10ca9b916b7b76d103899756fdabfa3ece0b3
SZenglein <13049685+SZenglein@users.noreply.github.com>
2023-12-12 23:01:02 +0100

    Turn off peripheral power in Battery critical shutdown just to be sure

--- src/Battery.cpp
+++ src/Battery.cpp
@@ -29,6 +29,8 @@ void Battery_Init(void) {
 		Battery_LogStatus();
 
 		Log_Println(batteryCriticalMsg, LOGLEVEL_NOTICE);
+		// turn off peripheral power just to be sure, even if it should not yet have been turned on
+		Power_PeripheralOff();
 		// Power down and enter deepsleep
 		// Don't call System_RequestSleep() here: If the battery is critial, we want to avoid as much init work as possible
 		// and also any blinking lights or sounds. The goal is to just stay off.
