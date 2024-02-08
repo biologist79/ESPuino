c3d79e5535192dd65dca909a8b60c91fd75583d7
Frederik Kriewitz <frederik@kriewitz.eu>
2024-02-08 10:21:58 +0100

    remove convertFilenameToAscii

--- src/Common.h
+++ src/Common.h
@@ -47,12 +47,6 @@ inline bool endsWith(const char *str, const char *suf) {
 	return b == suf && *a == *b;
 }
 
-inline void convertFilenameToAscii(String utf8String, char *asciiString) {
-	// Arduino >= 2.0.5 filenames are already unicode, copy to result here without UTF-8 decoding
-	strncpy(asciiString, (char *) utf8String.c_str(), utf8String.length() / sizeof(asciiString[0]));
-	asciiString[utf8String.length()] = 0;
-}
-
 inline void convertAsciiToUtf8(String asciiString, char *utf8String) {
 
 	int k = 0;
