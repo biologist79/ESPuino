04e462915d9e7bdaa0a298d141571ef192d8a102
Laszlo Hegedüs <laszlo.hegedues@gmail.com>
2024-02-21 20:49:10 +0100

    Add Playlist class (4/5): Embed playlist class into the SdCard

--- src/SdCard.cpp
+++ src/SdCard.cpp
@@ -7,6 +7,7 @@
 #include "Led.h"
 #include "Log.h"
 #include "MemX.h"
+#include "Playlist/BasicPlaylist.hpp"
 #include "System.h"
 
 #include <esp_random.h>
@@ -123,87 +124,6 @@ void SdCard_PrintInfo() {
 	Log_Printf(LOGLEVEL_NOTICE, sdInfo, cardSize, freeSize);
 }
 
-// Check if file-type is correct
-bool fileValid(const char *_fileItem) {
-	// clang-format off
-	// all supported extension
-	constexpr std::array audioFileSufix = {
-		".mp3",
-		".aac",
-		".m4a",
-		".wav",
-		".flac",
-		".ogg",
-		".oga",
-		".opus",
-		// playlists
-		".m3u",
-		".m3u8",
-		".pls",
-		".asx"
-	};
-	// clang-format on
-	constexpr size_t maxExtLen = strlen(*std::max_element(audioFileSufix.begin(), audioFileSufix.end(), [](const char *a, const char *b) {
-		return strlen(a) < strlen(b);
-	}));
-
-	if (!_fileItem || !strlen(_fileItem)) {
-		// invalid entry
-		return false;
-	}
-
-	// check for streams
-	if (strncmp(_fileItem, "http://", strlen("http://")) == 0 || strncmp(_fileItem, "https://", strlen("https://")) == 0) {
-		// this is a stream
-		return true;
-	}
-
-	// check for files which start with "/."
-	const char *lastSlashPtr = strrchr(_fileItem, '/');
-	if (lastSlashPtr == nullptr) {
-		// we have a relative filename without any slashes...
-		// set the pointer so that it points to the first character AFTER a +1
-		lastSlashPtr = _fileItem - 1;
-	}
-	if (*(lastSlashPtr + 1) == '.') {
-		// we have a hidden file
-		// Log_Printf(LOGLEVEL_DEBUG, "File is hidden: %s", _fileItem);
-		return false;
-	}
-
-	// extract the file extension
-	const char *extStartPtr = strrchr(_fileItem, '.');
-	if (extStartPtr == nullptr) {
-		// no extension found
-		// Log_Printf(LOGLEVEL_DEBUG, "File has no extension: %s", _fileItem);
-		return false;
-	}
-	const size_t extLen = strlen(extStartPtr);
-	if (extLen > maxExtLen) {
-		// extension too long, we do not care anymore
-		// Log_Printf(LOGLEVEL_DEBUG, "File not supported (extension to long): %s", _fileItem);
-		return false;
-	}
-	char extBuffer[maxExtLen + 1] = {0};
-	memcpy(extBuffer, extStartPtr, extLen);
-
-	// make the extension lower case (without using non standard C functions)
-	for (size_t i = 0; i < extLen; i++) {
-		extBuffer[i] = tolower(extBuffer[i]);
-	}
-
-	// check extension against all supported values
-	for (const auto &e : audioFileSufix) {
-		if (strcmp(extBuffer, e) == 0) {
-			// hit we found the extension
-			return true;
-		}
-	}
-	// miss, we did not find the extension
-	// Log_Printf(LOGLEVEL_DEBUG, "File not supported: %s", _fileItem);
-	return false;
-}
-
 // Takes a directory as input and returns a random subdirectory from it
 const String SdCard_pickRandomSubdirectory(const char *_directory) {
 	// Look if folder requested really exists and is a folder. If not => break.
@@ -252,27 +172,12 @@ const String SdCard_pickRandomSubdirectory(const char *_directory) {
 	return String();
 }
 
-static bool SdCard_allocAndSave(Playlist *playlist, const String &s) {
-	const size_t len = s.length() + 1;
-	char *entry = static_cast<char *>(x_malloc(len));
-	if (!entry) {
-		// OOM, free playlist and return
-		Log_Println(unableToAllocateMemForLinearPlaylist, LOGLEVEL_ERROR);
-		freePlaylist(playlist);
-		return false;
-	}
-	s.toCharArray(entry, len);
-	playlist->push_back(entry);
-	return true;
-};
-
-static std::optional<Playlist *> SdCard_ParseM3UPlaylist(File f, bool forceExtended = false) {
+static Playlist *SdCard_ParseM3UPlaylist(File f, bool forceExtended = false) {
 	const String line = f.readStringUntil('\n');
 	const bool extended = line.startsWith("#EXTM3U") || forceExtended;
-	Playlist *playlist = new Playlist();
+	Playlist *playlist = new BasicPlaylist(64);
 
 	// reserve a sane amount of memory to reduce heap fragmentation
-	playlist->reserve(64);
 	if (extended) {
 		// extended m3u file format
 		// ignore all lines starting with '#'
@@ -282,14 +187,11 @@ static std::optional<Playlist *> SdCard_ParseM3UPlaylist(File f, bool forceExten
 			if (!line.startsWith("#")) {
 				// this something we have to save
 				line.trim();
-				// save string
-				if (!SdCard_allocAndSave(playlist, line)) {
-					return std::nullopt;
-				}
+				// save entry
+				playlist->addMediaItem(MediaItem(line));
 			}
 		}
-		// resize std::vector memory to fit our count
-		playlist->shrink_to_fit();
+		playlist->compress();
 		return playlist;
 	}
 
@@ -298,23 +200,22 @@ static std::optional<Playlist *> SdCard_ParseM3UPlaylist(File f, bool forceExten
 	while (f.available()) {
 		String line = f.readStringUntil('\n');
 		// save string
-		if (!SdCard_allocAndSave(playlist, line)) {
-			return std::nullopt;
-		}
+		line.trim();
+		playlist->addMediaItem(MediaItem(line));
 	}
 	// resize memory to fit our count
-	playlist->shrink_to_fit();
+	playlist->compress();
 	return playlist;
 }
 
 /* Puts SD-file(s) or directory into a playlist
 	First element of array always contains the number of payload-items. */
-std::optional<Playlist *> SdCard_ReturnPlaylist(const char *fileName, const uint32_t _playMode) {
+Playlist *SdCard_ReturnPlaylist(const char *fileName, const uint32_t _playMode) {
 	// Look if file/folder requested really exists. If not => break.
 	File fileOrDirectory = gFSystem.open(fileName);
 	if (!fileOrDirectory) {
 		Log_Printf(LOGLEVEL_ERROR, dirOrFileDoesNotExist, fileName);
-		return std::nullopt;
+		return new BasicPlaylist();
 	}
 
 	Log_Printf(LOGLEVEL_DEBUG, freeMemory, ESP.getFreeHeap());
@@ -329,43 +230,32 @@ std::optional<Playlist *> SdCard_ReturnPlaylist(const char *fileName, const uint
 
 	// if we reach here, this was not a m3u
 	Log_Println(playlistGen, LOGLEVEL_NOTICE);
-	Playlist *playlist = new Playlist;
+
+	Playlist *playlist = new BasicPlaylist(64);
 
 	// File-mode
 	if (!fileOrDirectory.isDirectory()) {
-		if (!SdCard_allocAndSave(playlist, fileOrDirectory.path())) {
-			// OOM, function already took care of house cleaning
-			return std::nullopt;
-		}
+		playlist->addMediaItem(MediaItem(fileOrDirectory.path()));
+		playlist->compress();
 		return playlist;
 	}
 
-	// Directory-mode (linear-playlist)
-	playlist->reserve(64); // reserve a sane amount of memory to reduce the number of reallocs
-	size_t hiddenFiles = 0;
 	while (true) {
 		bool isDir;
-		const String name = fileOrDirectory.getNextFileName(&isDir);
-		if (name.isEmpty()) {
+		const String path = fileOrDirectory.getNextFileName(&isDir);
+		if (path.isEmpty()) {
+			// reached end of the folder enumeration
 			break;
 		}
 		if (isDir) {
+			// we ignore folders
 			continue;
 		}
-		// Don't support filenames that start with "." and only allow .mp3 and other supported audio file formats
-		if (fileValid(name.c_str())) {
-			// save it to the vector
-			if (!SdCard_allocAndSave(playlist, name)) {
-				// OOM, function already took care of house cleaning
-				return std::nullopt;
-			}
-		} else {
-			hiddenFiles++;
-		}
+		// no need to check against the rules here, MediaItem is doing it for us
+		playlist->addMediaItem(MediaItem(path));
 	}
-	playlist->shrink_to_fit();
-
+	playlist->compress();
 	Log_Printf(LOGLEVEL_NOTICE, numberOfValidFiles, playlist->size());
-	Log_Printf(LOGLEVEL_DEBUG, "Hidden files: %u", hiddenFiles);
+
 	return playlist;
 }
