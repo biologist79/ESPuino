d10eabffcfa900f4f128e8f6375d0255cc05ff9e
CaCO3 <caco@ruinelli.ch>
2024-04-19 23:14:50 +0200

    clang

--- src/Web.h
+++ src/Web.h
@@ -1,19 +1,18 @@
 #pragma once
 
 typedef enum class WebsocketCode {
-    Ok,
-    Error,
-    Dropout,
-    CurrentRfid,
-    Pong,
-    TrackInfo,
-    CoverImg,
-    Volume,
-    Settings,
-    Ssid,
-    TrackProgress
+	Ok = 0,
+	Error,
+	Dropout,
+	CurrentRfid,
+	Pong,
+	TrackInfo,
+	CoverImg,
+	Volume,
+	Settings,
+	Ssid,
+	TrackProgress
 } WebsocketCodeType;
-
 
 void Web_Cyclic(void);
 void Web_SendWebsocketData(uint32_t client, WebsocketCodeType code);
