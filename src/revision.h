ab4cf79425781514b540fc3f44d83893d8b24497
tueddy <d.carstensen@pixelplanet.de>
2024-04-25 20:40:07 +0200

    Software-revision: 20240425-1-DEV

--- src/revision.h
+++ src/revision.h
@@ -1,4 +1,4 @@
 #pragma once
 
 #include "gitrevision.h"
-constexpr const char softwareRevision[] = "Software-revision: 20240423-1-DEV";
+constexpr const char softwareRevision[] = "Software-revision: 20240425-1-DEV";
