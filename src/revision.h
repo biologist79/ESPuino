5a3738343b8d69ac1808400cad1a06cf34f0c7d2
tueddy <d.carstensen@pixelplanet.de>
2024-05-04 12:18:21 +0200

    Audio library, fix encoding of title see https://forum.espuino.de/t/eszett-ss-im-id3-tag-titel-macht-den-websocket-kaputt/2988

--- src/revision.h
+++ src/revision.h
@@ -1,4 +1,4 @@
 #pragma once
 
 #include "gitrevision.h"
-constexpr const char softwareRevision[] = "Software-revision: 20240425-1-DEV";
+constexpr const char softwareRevision[] = "Software-revision: 20240504-1-DEV";
