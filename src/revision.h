0cfec13ca135855fc1317bba88a0af5ec9d528af
tueddy <d.carstensen@pixelplanet.de>
2025-04-01 14:24:51 +0200

    add missing translations https://forum.espuino.de/t/bug-language-json/3642

--- src/revision.h
+++ src/revision.h
@@ -1,4 +1,4 @@
 #pragma once
 
 #include "gitrevision.h"
-constexpr const char softwareRevision[] = "Software-revision: 20250322-1-DEV";
+constexpr const char softwareRevision[] = "Software-revision: 20250401-1-DEV";
