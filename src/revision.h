ee224d7d068f751b237976000f57ccb48793bb62
tueddy <d.carstensen@pixelplanet.de>
2025-11-30 18:53:45 +0100

    Allocate tasklist in PSRAM if available

--- src/revision.h
+++ src/revision.h
@@ -1,4 +1,4 @@
 #pragma once
 
 #include "gitrevision.h"
-constexpr const char softwareRevision[] = "Software-revision: 20251129-1-DEV";
+constexpr const char softwareRevision[] = "Software-revision: 20251130-1-DEV";
